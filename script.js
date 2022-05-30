09504232ae24f179aedcb269071b79f7776348fc
Muslim <m.altamirov@mail.ru>
2022-05-30 15:42:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = `Alex`;
+
+// const obj = {
+// 	name: "John",
+// 	age: 25,
+// 	isMarried: false
+// };
+
+// // console.log(obj.name);
+// console.log(obj[name]);
+
+// const result = confirm("Are u here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "aye");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как вашa фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+
+// const storeName = 'olimp';
+// const Colleges = {
+// 	shop: 10000,
+// 	collegess: [
+// 		'James', 'Nick', 'Alice'
+// 	],
+// };
+
+let incr = 10,
+	decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(2*4 === 8);
